2bfdf819559004ad79a0b75dfdc2c302cab510fb
learn <learn@learn01.local>
2019-11-12 16:59:47 -0800

    this is whatever we have after class on monday

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -8,4 +8,18 @@ class BlogPostsController < ApplicationController
     @post = BlogPost.find(params[:id])
     #render "show.html.erb"
   end
+
+  def new
+      # render "new.html.erb"
+  end
+
+  def create
+      @post = BlogPost.create(title: params[:title], content: params[:content])
+
+      if @post.valid?
+          redirect_to @post
+      else
+          render action: :new
+      end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "blog_posts" => "blog_posts#index", as: "all_posts"
+  get "blog_posts/new" => "blog_posts#new", as: "new_posts"
   get "blog_posts/:id" => "blog_posts#show", as: "blog_post"
+  post "blog_posts" => "blog_posts#create"
   root to: 'blog_posts#index'
 end
